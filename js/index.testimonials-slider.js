539662c6660f7129fee2a4e9f97bac34755945e4
Illia Movchko <96948020+conceptbtw@users.noreply.github.com>
2023-07-12 21:31:19 +0000

    Big update js and css testimonials partial

---
+++ js/index.testimonials-slider.js
@@ -0,0 +1,24 @@
+$('.slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
