3f246d3dc1493b2440191816427170a4a04ee03e
AnastasiiaNychkalo <95881352+AnastasiiaNychkalo@users.noreply.github.com>
2023-07-21 10:48:05 +0000

    changed 6 task

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -41,14 +41,11 @@ function litres(time) {
 // Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
 
 function lovefunc(flower1, flower2){
-  if (flower1 % 2 === 0 && flower2 % 2 === 1) {
-    return true;
-  }
-  if (flower2 % 2 === 0 && flower1 % 2 === 1) {
-    return true;
+  if ((flower1 + flower2) % 2 === 0) {
+    return false;
   }
   else {
-    return false;
+    return true;
   }
 }
 
