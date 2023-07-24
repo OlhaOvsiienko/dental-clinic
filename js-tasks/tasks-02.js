7a1b5f02fca148e01d16e47ccb43a9ec0e246c11
Людмила Кирюшина <o.v.ovsiyenko@gmail.com>
2023-07-24 18:08:07 +0300

    corrected wolf and sheep

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -49,14 +49,12 @@ function min(arr, toReturn) {
 // https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
 function warnTheSheep(queue) {
   const indexOfWolf = queue.indexOf("wolf");
-  for (i = 0; i < queue.length; i ++) {
-
   if (indexOfWolf === queue.length - 1) {
     return "Pls go away and stop eating my sheep";
-  } else {
-    return `Oi! Sheep number ${indexOfWolf + 1}! You are about to be eaten by a wolf!`;
   }
-}
+  return `Oi! Sheep number ${
+    queue.length - indexOfWolf - 1
+  }! You are about to be eaten by a wolf!`;
 }
 
 // Task 2 (additional)
