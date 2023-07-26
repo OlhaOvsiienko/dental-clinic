bd4ae14c4b8d7f8668d0a6ea12566472eb1d2a4e
MarkhevraJurii <139385682+MarkhevraJurii@users.noreply.github.com>
2023-07-26 09:00:15 +0000

    Comment

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,7 +1,31 @@
-function makeNegative(num) {
-    if (num > 0) {
-  return -num;
-  } else {
-  return num;
+1. https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+function stringToArray(string){
+  return string.split(" ");
+ }
+
+ 2. https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+ function DNAtoRNA(dna) {
+  return dna.replace(/T/g, 'U');
+}
+
+3. https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+var min = function(list) {
+  var i;
+for (i = 0; i < list.length; i ++) {
+  if (list[i] < list[0]) {
+    list[0] = list[i];
   }
+}
+return list[0];
+}
+var max = function(list) {
+  var i;
+for (i = 0; i < list.length; i ++) {
+  if (list[i] > list[0]) {
+    list[0] = list[i];
   }
+}
+return list[0];
+}
+
+4. https://www.codewars.com/kata/find-the-first-non-consecutive-number/train/javascript
