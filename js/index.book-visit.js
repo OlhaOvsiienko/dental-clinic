8ab39e143c4722b71031c5ad179b98ea2c8a53ec
Людмила Кирюшина <o.v.ovsiyenko@gmail.com>
2023-07-27 15:44:45 +0300

    edit index.book-visit.js

---
+++ js/index.book-visit.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    alert("Congratulations! You have successfully scheduled your visit!");
+  }
+   const sendButton = document.getElementById("sendButton");
+  sendButton.addEventListener("click", showMessage);
--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
 document.addEventListener('partialsLoaded', () => {
     import('../js/footer.js');
+    import ('../js/index.book-visit.js');
 });
