fac39d9bcaa56bc2389187ea3880dffc39d0b07a
Людмила Кирюшина <o.v.ovsiyenko@gmail.com>
2023-07-27 21:46:50 +0300

    edit index.book-visit.js and index.book-visit-confirmation

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
-document.addEventListener('partialsLoaded', () => {
-    import('../js/footer.js');
-    import ('../js/index.book-visit.js');
-});
+function validateForm() {
+    const name = document.getElementById("name").value;
+    const phone = document.getElementById("phone").value;
+    const dentist = document.getElementById("dentist").value;
+    const clinic = document.getElementById("clinic").value;
+  
+    if (
+      name.trim() === "" ||
+      phone.trim() === "" ||
+      dentist === "" ||
+      clinic === ""
+    ) {
+      alert("Fill in required fields");
+      return false;
+    }
+    return true;
+  }
