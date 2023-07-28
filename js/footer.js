b12ec3ebe4ddc8843b2522878e65af3c2f95b9de
MarkhevraJurii <139385682+MarkhevraJurii@users.noreply.github.com>
2023-07-28 04:32:58 +0000

    Comment

--- js/footer.js
+++ js/footer.js
@@ -1,12 +1,6 @@
-// function callNumber () {
-//     let phoneNumber = document.getElementById('phone-number').value;
-//     window.open('tel:${phoneNumber}');
-// }
+function callNumber () {
+    const phoneNumber = document.getElementById('phone-number').value;
+    window.open('tel:${phoneNumber}');
+}
 
-// document.getElementById('phone-number').addEventListener("click", callNumber);
-
-
-// debugger;
-
-const pNV = document.getElementById('phone-number');
-pNV.style.color = "red";
+document.getElementById('phone-number').addEventListener("click", callNumber);
--- js/index.js
+++ js/index.js
@@ -1,8 +1,5 @@
 document.addEventListener('partialsLoaded', () => {
     import('../js/footer.js');
-<<<<<<< HEAD
-    import('../js/header-nav.js');
-=======
+    import('../js/header.js');
     import ('../js/index.book-visit.js');
->>>>>>> 8ab39e143c4722b71031c5ad179b98ea2c8a53ec
 });
