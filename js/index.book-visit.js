2031369e99ebb81c4dc706d0de28e6f87ec0ce5b
Людмила Кирюшина <o.v.ovsiyenko@gmail.com>
2023-07-28 14:46:33 +0300

    edit booking

--- js/index.book-visit.js
+++ js/index.book-visit.js
@@ -1,21 +1,19 @@
-
-
- function validateForm() {
+function validateForm() {
   const name = document.getElementById("name").value;
   const phone = document.getElementById("phone").value;
   const dentist = document.getElementById("dentist").value;
   const clinic = document.getElementById("clinic").value;
-  
+
   document.getElementById("errorMessage").style.display = "none";
   document.getElementById("errorMessagePhone").style.display = "none";
   document.getElementById("errorMessageName").style.display = "none";
- 
-  if (name.trim() === "" ) {
+
+  if (name.trim() === "") {
     document.getElementById("errorMessageName").classList.add("active");
     return false;
-    } else if (phone.trim() === "") {
-      document.getElementById("errorMessagePhone").style.display = "block";
-      return false;
+  } else if (phone.trim() === "") {
+    document.getElementById("errorMessagePhone").style.display = "block";
+    return false;
   } else if (phone.replace(/\D/g, "").length !== 12) {
     document.getElementById("errorMessagePhone").style.display = "block";
     return false;
@@ -26,5 +24,5 @@
     document.getElementById("errorMessage").style.display = "block";
     return false;
   }
-     return true;
-} 
+  return true;
+}
