12a2314ca5ae2a7a47b4c22d1325cd1072728219
Людмила Кирюшина <o.v.ovsiyenko@gmail.com>
2023-08-01 00:13:08 +0300

    add shedule.html

---
+++ js/shedule.js
@@ -0,0 +1,8 @@
+document.addEventListener('partialsLoaded', () => {
+    import('./header-nav.js');
+    import('./header.js');
+    import('./shedule.clinics.js');
+    import('./shedule.visit.js');
+    import('./shedule.terms-conditions.js');
+    import('./footer.js');
+  });
