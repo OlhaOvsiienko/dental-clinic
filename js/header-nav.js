0b952549130df09dc40d778d0d16214068a05e3e
MarkhevraJurii <139385682+MarkhevraJurii@users.noreply.github.com>
2023-08-01 13:50:25 +0000

    Header

--- js/header-nav.js
+++ js/header-nav.js
@@ -2,13 +2,13 @@ const menu = document.querySelector('.mobile-menu');
 const menuIcon = document.querySelector('.hamburger');
 const menuIconItem = document.querySelector('.bar');
 const menuContent = document.querySelector('.content-menu');
-
 menuIcon.addEventListener("click", showMenu);
 
 function showMenu() { 
     menu.classList.toggle('active');
     menuIconItem.classList.toggle('active');
     menuContent.classList.toggle('active');
+    
 }
 
 
