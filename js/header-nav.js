725cb7c56059cc073f4f8a5c273fb3c205e8ec2e
MarkhevraJurii <139385682+MarkhevraJurii@users.noreply.github.com>
2023-08-03 13:23:13 +0000

    mobile menu btn

--- js/header-nav.js
+++ js/header-nav.js
@@ -1,15 +1,28 @@
-const menu = document.querySelector('.mobile-menu');
-const menuIcon = document.querySelector('.hamburger');
-const menuIconItem = document.querySelector('.bar');
+// const menu = document.querySelector('.mobile-menu');
+// const menuIcon = document.querySelector('.menu-btn');
+// const menuIconItem = document.querySelector('.bar');
+// const menuContent = document.querySelector('.content-menu');
+// const bodyLock = document.querySelector('body');
+// menuIcon.addEventListener("click", showMenu);
+
+// function showMenu() { 
+//     menu.classList.toggle('active');
+//     menuIconItem.classList.toggle('active');
+//     menuContent.classList.toggle('active');
+//     menuIcon.classList.toggle('active');
+//     bodyLock.classList.toggle('lock');
+// }
+
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuIcon = document.querySelector('.menu-btn');
 const menuContent = document.querySelector('.content-menu');
+const bodyLock = document.querySelector('body');
 menuIcon.addEventListener("click", showMenu);
 
-function showMenu() { 
-    menu.classList.toggle('active');
-    menuIconItem.classList.toggle('active');
-    menuContent.classList.toggle('active');
-    
+function showMenu() {
+mobileMenu.classList.toggle('active');
+menuBtn.classList.toggle('active');
+menuContent.classList.toggle('active');
+bodyLock.classList.toggle('lock');
 }
-
-
 
