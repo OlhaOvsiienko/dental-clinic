92e9a09536890b1815c4a20a39a484ec7edc1114
MarkhevraJurii <139385682+MarkhevraJurii@users.noreply.github.com>
2023-08-03 13:44:19 +0000

    hav, img

--- js/header-nav.js
+++ js/header-nav.js
@@ -1,23 +1,8 @@
-// const menu = document.querySelector('.mobile-menu');
-// const menuIcon = document.querySelector('.menu-btn');
-// const menuIconItem = document.querySelector('.bar');
-// const menuContent = document.querySelector('.content-menu');
-// const bodyLock = document.querySelector('body');
-// menuIcon.addEventListener("click", showMenu);
-
-// function showMenu() { 
-//     menu.classList.toggle('active');
-//     menuIconItem.classList.toggle('active');
-//     menuContent.classList.toggle('active');
-//     menuIcon.classList.toggle('active');
-//     bodyLock.classList.toggle('lock');
-// }
-
 const mobileMenu = document.querySelector('.mobile-menu');
-const menuIcon = document.querySelector('.menu-btn');
+const menuBtn = document.querySelector('.menu-btn');
 const menuContent = document.querySelector('.content-menu');
 const bodyLock = document.querySelector('body');
-menuIcon.addEventListener("click", showMenu);
+menuBtn.addEventListener("click", showMenu);
 
 function showMenu() {
 mobileMenu.classList.toggle('active');
