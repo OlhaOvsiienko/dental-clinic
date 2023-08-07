d9c5546b7f76e2be459aa732fc3a08bf7a105129
MarkhevraJurii <139385682+MarkhevraJurii@users.noreply.github.com>
2023-08-07 16:14:10 +0000

    footers js

--- js/footer.js
+++ js/footer.js
@@ -1,8 +1,16 @@
+document.querySelector('.footer-phone-description-link').addEventListener("click", callNumber);
+document.querySelector('.footer-phone-LA-description-link').addEventListener("click", callNumber);
+
 function callNumber () {
     const phoneNumber = document.getElementById('phone-number').value;
     window.open('tel:${phoneNumber}');
 }
 
-document.getElementById('phone-number').addEventListener("click", callNumber);
 
+
+
+// function callNumberLA () {
+//     const phoneNumberLA = document.getElementById('phone-numberLA').value;
+//     window.open('tel:${phoneNumber}');
+// }
 
