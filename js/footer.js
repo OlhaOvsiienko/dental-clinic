0f998ab95f21bde89dc299264099c827b956f0ca
MarkhevraJurii <139385682+MarkhevraJurii@users.noreply.github.com>
2023-08-09 15:05:08 +0000

    fixed footer js

--- js/footer.js
+++ js/footer.js
@@ -1,15 +1,28 @@
-function callNY () {
-    const clinicPhoneNumber = document.getElementById('phone-numberNY').innerHTML;
-    window.open('tel:${clinicPhoneNumber}');
-}
+// function callNY () {
+//     const phoneNumberNY = document.getElementById('phone-numberNY').textContent;
+//     window.open('tel:${phoneNumberNY}');
+// }
 
-document.querySelector('.footer-phone-NY-description-link').addEventListener("click", callNY);
+// document.querySelector('.footer-phone-NY-description-link').addEventListener("click", callNY);
 
-function callLA () {
-    const clinicPhoneNumber = document.getElementById('phone-numberLA').innerHTML;
-    window.open('tel:${clinicPhoneNumber}');
-}
+// function callLA () {
+//     const phoneNumberLA = document.getElementById('phone-numberLA').textContent;
+//     window.open('tel:${phoneNumberLA}');
+// }
 
-document.querySelector('.footer-phone-LA-description-link').addEventListener("click", callLA);
+// document.querySelector('.footer-phone-LA-description-link').addEventListener("click", callLA);
 
 
+function callNumberNY() {
+    const clinicPhoneNumber = document.getElementById('ny-clinic-phone-number').textContent;
+    window.open(`tel:${clinicPhoneNumber}`);
+  }
+  
+  document.getElementById('ny-clinic-phone-number').addEventListener("click", callNumberNY);
+  
+  function callNumberLA() {
+    const clinicPhoneNumber = document.getElementById('la-clinic-phone-number').textContent;
+    window.open(`tel:${clinicPhoneNumber}`);
+  }
+  
+  document.getElementById('la-clinic-phone-number').addEventListener("click", callNumberLA);
