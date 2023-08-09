1264e122fddf21f7b889f9677f51fd40631bfff1
Людмила Кирюшина <o.v.ovsiyenko@gmail.com>
2023-08-09 15:48:48 +0300

    edit schedule.clinics

--- js/schedule.clinics.js
+++ js/schedule.clinics.js
@@ -1,29 +1,30 @@
 // PhoneCall
-function callNumberNY () {
-  const clinicPhoneNumber = document.getElementById('ny-clinic-phone-number').value;
-  window.open('tel:${clinicPhoneNumber}');
+function callNumberNY() {
+  const clinicPhoneNumber = document.getElementById('ny-clinic-phone-number').textContent;
+  window.open(`tel:${clinicPhoneNumber}`);
 }
 
 document.getElementById('ny-clinic-phone-number').addEventListener("click", callNumberNY);
 
-function callNumberLA () {
-  const clinicPhoneNumber = document.getElementById('la-clinic-phone-number').value;
-  window.open('tel:${clinicPhoneNumber}');
+function callNumberLA() {
+  const clinicPhoneNumber = document.getElementById('la-clinic-phone-number').textContent;
+  window.open(`tel:${clinicPhoneNumber}`);
 }
 
 document.getElementById('la-clinic-phone-number').addEventListener("click", callNumberLA);
 
-//Email
+// Email
 function emailNY() {
-  const clinicEmail = document.getElementById('ny-clinic-email').href;
+  const clinicEmail = document.getElementById('ny-clinic-email').getAttribute('href');
   window.location.href = clinicEmail;
 }
 
 document.getElementById('ny-clinic-email').addEventListener("click", emailNY);
 
 function emailLA() {
-  const clinicEmail = document.getElementById('la-clinic-email').href;
+  const clinicEmail = document.getElementById('la-clinic-email').getAttribute('href');
   window.location.href = clinicEmail;
 }
 
 document.getElementById('la-clinic-email').addEventListener("click", emailLA);
+
