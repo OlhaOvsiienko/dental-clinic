2e698bcae67d8a63a714a96b1c33650362e1d594
OlhaOvsiienko <o.v.ovsiyenko@gmail.com>
2024-08-16 17:45:05 +0300

    Initial commit

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 document.addEventListener('partialsLoaded', () => {
-  import('./header-nav.js');
-  import('./header.js');
-  import('./index.testimonials-slider.js');
-  import('./index.news-and-articles.js');
-  import('./services.js');
+  
   import('./index.book-visit.js');
-  import('./footer.js');
+  
   import('./preventDefault.js')
 });
--- js/schedule.js
+++ js/schedule.js
@@ -1,8 +1,5 @@
 document.addEventListener('partialsLoaded', () => {
-    import('./header-nav.js');
-    import('./header.js');
     import('./schedule.clinics.js');
     import('./schedule.visit.js');
     import('./schedule.terms-conditions.js');
-    import('./footer.js');
   });
